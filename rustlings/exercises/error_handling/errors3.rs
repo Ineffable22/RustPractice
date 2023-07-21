ec95dfc5b42d4130607de3b47c96c33ad0b54a21
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-07-21 12:39:34 -0500

    Feat(Task51-56): Error_handling Completed

--- rustlings/exercises/error_handling/errors3.rs
+++ rustlings/exercises/error_handling/errors3.rs
@@ -4,28 +4,37 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(), ParseIntError>{
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
-
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+    let cost = total_cost(pretend_user_input);
+    match cost {
+        Ok(value) => {
+            if value > tokens {
+                println!("You can't afford that many!");
+            } else {
+                tokens -= value;
+                println!("You now have {} tokens.", tokens);
+            }
+            Ok(())
+        },
+        Err(value) => Err(value),
     }
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>()?;
+    let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    // Ok(qty * cost_per_item + processing_fee)
+    match qty {
+        Ok(value) => Ok(value * cost_per_item + processing_fee),
+        Err(value) => Err(value),
+    }
 }
