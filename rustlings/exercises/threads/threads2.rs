8b858dcce495cc89806797a6b1b5ea3e5f995e58
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-07-23 18:59:39 -0500

    Feat(Task76-79): Threads Completed

--- rustlings/exercises/threads/threads2.rs
+++ rustlings/exercises/threads/threads2.rs
@@ -3,9 +3,7 @@
 // Building on the last exercise, we want all of the threads to complete their work but this time
 // the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -14,21 +12,26 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
     let mut handles = vec![];
+    let counter = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
+    // let status = Arc::new(JobStatus { jobs_completed: 0 });
     for _ in 0..10 {
-        let status_shared = Arc::clone(&status);
+        // let status_shared = Arc::clone(&status);
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            let mut num = counter.lock().unwrap();
+            num.jobs_completed += 1;
         });
+
         handles.push(handle);
     }
     for handle in handles {
         handle.join().unwrap();
         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
         // interesting in the output? Do you have to 'join' on all the handles?
-        println!("jobs completed {}", ???);
+        let num = counter.lock().unwrap();
+        println!("jobs completed {}", num.jobs_completed);
     }
 }
--- rustlings/exercises/threads/threads3.rs
+++ rustlings/exercises/threads/threads3.rs
@@ -1,10 +1,8 @@
 // threads3.rs
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -25,22 +23,25 @@ impl Queue {
 }
 
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
+    let qc = Arc::new(Mutex::new(q));
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    let tx2 = tx.clone();
 
     thread::spawn(move || {
+        let qc1 = qc1.lock().unwrap();
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            &tx.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
     thread::spawn(move || {
+        let qc2 = qc2.lock().unwrap();
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
