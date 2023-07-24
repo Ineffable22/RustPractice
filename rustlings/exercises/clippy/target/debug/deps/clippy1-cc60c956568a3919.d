dadee8f43489bd66fdd0311f695e1655f54268d8
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-07-23 20:04:44 -0500

    Feat(Task88-91): Clippy Completed

--- rustlings/exercises/clippy/target/debug/deps/clippy1-cc60c956568a3919.d
+++
@@ -1,8 +0,0 @@
-/home/ineffable/RustPractice/rustlings/exercises/clippy/target/debug/deps/clippy1-cc60c956568a3919.rmeta: clippy1.rs Cargo.toml
-
-/home/ineffable/RustPractice/rustlings/exercises/clippy/target/debug/deps/clippy1-cc60c956568a3919.d: clippy1.rs Cargo.toml
-
-clippy1.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
---
+++ rustlings/exercises/clippy/target/debug/deps/clippy3-fa85db06c18a41d3.d
@@ -0,0 +1,8 @@
+/home/ineffable/RustPractice/rustlings/exercises/clippy/target/debug/deps/clippy3-fa85db06c18a41d3.rmeta: clippy3.rs Cargo.toml
+
+/home/ineffable/RustPractice/rustlings/exercises/clippy/target/debug/deps/clippy3-fa85db06c18a41d3.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
